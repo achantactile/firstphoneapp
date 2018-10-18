02c059f0db9767615f06de7d5895b237996cc4ca
Angela Chan <a.chan@thetactilegroup.com>
2018-10-18 18:13:24 -0400

    init project

---
+++ Hello World/Hello World/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by Angela Chan on 10/18/18.
+//  Copyright © 2018 Angela Chan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
